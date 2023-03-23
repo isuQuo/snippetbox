d8b932ca034a32a9b7a7a42f9c8171f5fb4c65d7
isuQuo <isuquo@gmail.com>
2023-03-23 16:04:06 +0800

    Added two new HTTP handler functions for displaying a specific snippet and creating a new snippet respectively. Added new URL patterns "/snippet/view" and "/snippet/create" to the ServeMux and registered the corresponding functions as their handlers. In the home function, added a check to return a 404 error if the requested URL path is not "/". In the snippetCreate function, added a check to return a 405 error if the HTTP method used is not POST, and set the Allow header to indicate that only POST is allowed. Changed the server address in the ListenAndServe function from using a wildcard IP address to using a specific IP address (127.0.0.1) for security reasons.

--- main.go
+++ main.go
@@ -7,15 +7,37 @@ import (
 
 // home is a simple HTTP handler function which writes a response.
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello World!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	// Create a new ServeMux and register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe("127.0.0.1:8080", mux)
 	log.Fatal(err)
 }
