79868bc6d21c09c1c0704ceccce69059d26ab29d
isuQuo <isuquo@gmail.com>
2023-03-27 15:41:43 +0700

    added dummy logic for creating a new entry in the snippetbox table. Fires when visiting the create endpoint.

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -18,7 +18,23 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content string, expires int) (int, error) {
-	return 0, nil
+	query := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	// Use the Exec() method on the embedded connection pool to execute our
+	result, err := m.DB.Exec(query, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the LastInsertId() method on the result object to get the ID of the
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// Return the ID of the newly inserted record.
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
