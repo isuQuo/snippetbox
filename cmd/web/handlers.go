b3c5edbedc0eba71ecce587c9358e938a9deb779
isuQuo <isuquo@gmail.com>
2023-04-07 00:01:44 +1000

    created signout functionality and edited nav bar depending on user session status

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -234,5 +234,14 @@ func (app *application) signinUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signoutUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Logout the user")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
