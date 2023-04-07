256db99c35d9f506b7b668295dcc7d4935b1c590
isuQuo <isuquo@gmail.com>
2023-04-07 19:34:43 +1000

    check if user is authenticated via request context

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,17 +8,6 @@ import (
 )
 
 func (app *application) routes() http.Handler {
-	/* // Create a new ServeMux and register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static/" directory.
-	//fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// Use the mux.Handle() function to register the file server as the handler for all URL paths that start with "/static/".
-	//mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate) */
-
 	// Create a new httprouter instance.
 	router := httprouter.New()
 
@@ -31,7 +20,7 @@ func (app *application) routes() http.Handler {
 	//fileServer := http.FileServer(http.Dir("./ui/static/"))
 	//router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave, app.noSurf)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, app.noSurf, app.authenticate)
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
